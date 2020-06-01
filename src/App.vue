725a5f2678f721118487dadf401e23e273bf107e
Alex Wang <alexwangnj@gmail.com>
2020-06-01 13:33:09 -0400

    Remove navbar and add container

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/interval-trainer">Interval Trainer</router-link>
-    </div>
+    <div class="container">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/interval-trainer">Interval Trainer</router-link>
+
     <router-view/>
+    </div>
   </div>
 </template>
 
