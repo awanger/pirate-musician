5dae4febdd17b7d400e3ca900f01403a46346380
Alex Wang <alexwangnj@gmail.com>
2020-06-12 18:20:50 -0400

    Create two separate tests

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -4,9 +4,13 @@ describe('Basic Test', () => {
     cy.get('#nav > a:nth-child(2)').click() // click link to interval trainer
     cy.get('#settings').should('be.visible')
     cy.get('.btn-play').should('be.visible')
+
+  })
+
+  it('checking an answer enables action button', ()=> {
     cy.get('.btn-action').should('be.disabled') // the check button should be disabled before selecting any answer
     cy.get('.btn-answer').eq(4).click()
-    cy.get('.btn-action').should('not.be.disabled') // the check button should be enabled after checking an answer
-    
+    cy.get('.btn-answer').eq(4).should('have.class', 'selected')
+    cy.get('.btn-action').should('not.be.disabled').click()
   })
 })
