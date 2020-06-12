d290d8e09a323d31e9e4e8d7b19a149fd978288b
Alex Wang <alexwangnj@gmail.com>
2020-06-12 18:01:41 -0400

    Expand test to make sure button remains disabled when it's supposed to

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,6 +1,9 @@
-describe('The Home Page', () => {
+describe('Basic Test', () => {
   it('successfully loads', () => {
     cy.visit('/') // change URL to match your dev URL
-    cy.get('#nav').find('a') // check if there's a link to the Interval trainer
+    cy.get('#nav > a:nth-child(2)').click() // click link to interval trainer
+    cy.get('.btn-action').should('be.disabled') // the check button should be disabled before selecting any answer
+    cy.get('.btn-answer').eq(4).click()
+    cy.get('.btn-action').should('not.be.disabled') // the check button should be disabled before selecting any answer
   })
 })
