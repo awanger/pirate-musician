4d09b35fee3f0b1249e65c72c460c1878a8d1f34
Alex Wang <alexwangnj@gmail.com>
2020-06-12 18:13:09 -0400

    Add test to make sure play button is on the page

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -2,8 +2,11 @@ describe('Basic Test', () => {
   it('successfully loads', () => {
     cy.visit('/') // change URL to match your dev URL
     cy.get('#nav > a:nth-child(2)').click() // click link to interval trainer
+    cy.get('#settings').should('be.visible')
+    cy.get('.btn-play').should('be.visible')
     cy.get('.btn-action').should('be.disabled') // the check button should be disabled before selecting any answer
     cy.get('.btn-answer').eq(4).click()
-    cy.get('.btn-action').should('not.be.disabled') // the check button should be disabled before selecting any answer
+    cy.get('.btn-action').should('not.be.disabled') // the check button should be enabled after checking an answer
+    
   })
 })
