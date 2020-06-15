6c1c58b63aefe37f91c5cbbedd51235283b70370
Alex Wang <alexwangnj@gmail.com>
2020-06-15 17:53:39 -0400

    Add the outlines for some new tests

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,16 +1,44 @@
+import { getters } from '../../src/store/store.js'
+
 describe('Basic Test', () => {
-  it('successfully loads', () => {
+  const quizContext = getters.quizService.machine.context
+  function getRandomInteger(min=0, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  before(() => {
+    // check if the import worked correctly
+    expect(quizContext).to.be.a('object')
     cy.visit('/') // change URL to match your dev URL
+  })
+
+  it('successfully loads', () => {
     cy.get('#nav > a:nth-child(2)').click() // click link to interval trainer
     cy.get('#settings').should('be.visible')
     cy.get('.btn-play').should('be.visible')
-
+    expect(quizContext.selectedAnswer).to.be.null
   })
 
   it('checking an answer enables action button', ()=> {
     cy.get('.btn-action').should('be.disabled') // the check button should be disabled before selecting any answer
-    cy.get('.btn-answer').eq(4).click()
-    cy.get('.btn-answer').eq(4).should('have.class', 'selected')
-    cy.get('.btn-action').should('not.be.disabled').click()
+    cy.get('.btn-answer').eq(0).click().should('have.class', 'selected')
+    cy.get('.btn-action').should('not.be.disabled')
   })
+
+  it('checking another answer should remain in checked state', ()=> {
+    var answerButtons = cy.get('.btn-answer')
+    answerButtons.eq(5).click().should('have.class','selected')
+
+  })
+
+  it('getting correct answer should take you to correct state', ()=> {
+    var answerButtons = cy.get('.btn-answer')
+    
+  })
+
+  it('getting incorrect answer should take you to correct state', ()=> {
+    var answerButtons = cy.get('.btn-answer')
+    
+  })
+
 })
