cf2bf001c1733f76261132eae8541f8a8ccc13e8
Alex Wang <alexwangnj@gmail.com>
2020-07-02 17:03:11 -0400

    Refactor conditional guards in the machine

--- src/machine.js
+++ src/machine.js
@@ -1,19 +1,7 @@
 import { Machine, assign } from 'xstate';
 import questions from '@/store/questions';
 
-// conditional guards
-// const isCorrect = ({ age }) => age >= 18;
-// const isWrong = ({ age }) => age < 18;  
-const quizCompleted = (context) => context.currentQuestionIndex === context.totalNumQuestions;
-const fromActionButton = (_, event) => {
-  // console.log(event.selectedButton.target.classList);
-  return event.selectedButton.target.classList.contains('btn-action');
-}
-const isCorrect = (context) => {
-  return context.selectedAnswer === context.currentQuestion.correctAnswer;
-}
-  
-// create Interval Trainer machine
+
 const quizMachine = Machine({
   id: 'quiz',
   context: {
@@ -27,7 +15,7 @@ const quizMachine = Machine({
     newQuestion: {
       entry: ['nextQuestion', 'resetSelectedAnswer'],
       on: { '': [
-            { target: 'complete', cond: quizCompleted},
+            { target: 'complete', cond: 'quizCompleted'},
             { target: 'displayQuestion' }
         ] }
     },
@@ -42,7 +30,7 @@ const quizMachine = Machine({
       on: {
         CLICK: [
           { target: 'evaluate', 
-            cond: fromActionButton // if the click event is from the action button, then evaluate the answer
+            cond: 'fromActionButton' // if the click event is from the action button, then evaluate the answer
           }, 
           { target: 'checked',
             actions: assign({ selectedAnswer: (context, event) => context.selectedAnswer = event.selectedButton.target.dataset.interval })
@@ -54,16 +42,16 @@ const quizMachine = Machine({
     evaluate: {
       on: {
         '': [
-          { target: 'correct', cond: isCorrect },
+          { target: 'correct', cond: 'isCorrect' },
           { target: 'incorrect' }
         ]
       }
     },
     correct: {
-      on: { CLICK: { target:'newQuestion', cond: fromActionButton } }
+      on: { CLICK: { target:'newQuestion', cond: 'fromActionButton' } }
     },
     incorrect: {
-      on: { CLICK: { target:'displayQuestion', cond: fromActionButton } }
+      on: { CLICK: { target:'displayQuestion', cond: 'fromActionButton' } }
     },
     complete: {
       entry: ['resetSelectedAnswer'],
@@ -76,6 +64,16 @@ const quizMachine = Machine({
     loadQuestion: assign({ currentQuestion: context => questions[context.currentQuestionIndex] }),
     nextQuestion: assign( { currentQuestionIndex: context => context.currentQuestionIndex + 1 }),
     resetSelectedAnswer: assign( { selectedAnswer: context => context.selectedAnswer = null })
+  },
+  guards: {
+    isCorrect: (context) => {
+      return context.selectedAnswer === context.currentQuestion.correctAnswer;
+    },
+    fromActionButton: (_, event) => {
+      // console.log(event.selectedButton.target.classList);
+      return event.selectedButton.target.classList.contains('btn-action');
+    },
+    quizCompleted: (context) => { return context.currentQuestionIndex === context.totalNumQuestions; }
   }
 });
 
