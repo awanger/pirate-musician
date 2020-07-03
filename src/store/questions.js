c0885d4ab50fb38016e0d3684464b6e6b79567a8
Alex Wang <alexwangnj@gmail.com>
2020-07-03 18:17:53 -0400

    Refactor the generateInterval() function

--- src/store/questions.js
+++ src/store/questions.js
@@ -1,9 +1,9 @@
 class Note {
-  constructor(pitch, startTime, endTime) {
+  constructor(pitch, startStep, endStep) {
     this.pitch = pitch;
-    this.startTime = startTime;
-    this.endTime = endTime;
-    this.velocity = 110;
+    this.quantizedStartStep = startStep;
+    this.quantizedEndStep = endStep;
+    this.velocity = 100;
   }
   getPitch() {
     return this.pitch;
@@ -26,10 +26,11 @@ class Interval {
 }
 
 class Question {
-  constructor(notes, correctAnswer) {
-    this.notes = notes; // I presume an array of notes
+  constructor(notes, correctAnswer, tempo=60) {
+    this.notes = notes;
     this.correctAnswer = correctAnswer;
-    this.totalTime = 4;
+    this.quantizationInfo = {stepsPerQuarter: 1};
+    this.tempos = [{time:0, qpm: tempo}];
   }
 
   // helper function
@@ -39,19 +40,39 @@ class Question {
     return Math.floor(Math.random()*(max - min) + min)
   }
 
-  static generateRandomInterval(min_pitch, max_pitch) {
-    const OCTAVE_RANGE = 12;
-    const referencePitch = this.getRandomInteger(min_pitch, max_pitch)
+  static generateInterval(min_pitch, max_pitch, intervalStr="Octave", isAscending=true) {
+    var interval_to_pitch = {
+      "Unison":0,
+      "m2":1,
+      "M2":2,
+      "m3":3,
+      "M3":4,
+      "P4":5,
+      "Tritone":6,
+      "P5":7,
+      "m6":8,
+      "M6":9,
+      "m7":10,
+      "M7":11,
+      "Octave":12 
+    }
+    var referencePitch = this.getRandomInteger(min_pitch, max_pitch);
+    var secondPitch = referencePitch;
 
-    var firstNote = new Note(referencePitch, 0.0, 1.0);
-    var secondNote = new Note(this.getRandomInteger(referencePitch-OCTAVE_RANGE, referencePitch+OCTAVE_RANGE),
-                              1.0, 2.0); // might get a unison and don't forget about tritone
+    if(isAscending) {
+      secondPitch += interval_to_pitch[intervalStr];
+    } else {
+      secondPitch -= interval_to_pitch[intervalStr];
+    }
+
+    var firstNote = new Note(referencePitch, 0, 1);
+    var secondNote = new Note(secondPitch,1,2);
 
     var interval = new Interval(firstNote, secondNote);
     return interval
   }
 
-  static calcCorrectAnswer(interval) {
+  static pitchToInterval(interval) {
     var pitch_dict = {
       0:"Unison",
       1:"m2",
@@ -75,14 +96,22 @@ class Question {
   }
 }
 
+/*
+
+function generateQuestions(str[] intervalsSelected, int playbackTempo, int numOfQuestions, str intervalDirection, str playbackStyle) {
+
+}
+
+*/
+
 let questions = []
 let NUM_OF_QUESTIONS = 4;
-const LOWEST_PITCH = 40;
+const LOWEST_PITCH = 48;
 const HIGHEST_PITCH = 76;
 for(var i=0; i<NUM_OF_QUESTIONS; i++) {
-  let randomInterval = Question.generateRandomInterval(LOWEST_PITCH, HIGHEST_PITCH);
-  let correctAnswer = Question.calcCorrectAnswer(randomInterval);
-  let question = new Question([randomInterval.getFirstNote(), randomInterval.getSecondNote()], correctAnswer, 4); // keep it like this for now
+  let randomInterval = Question.generateInterval(LOWEST_PITCH, HIGHEST_PITCH);
+  let correctAnswer = Question.pitchToInterval(randomInterval);
+  let question = new Question([randomInterval.getFirstNote(), randomInterval.getSecondNote()], correctAnswer, 60); // keep it like this for now
   questions.push(question);
 }
 
