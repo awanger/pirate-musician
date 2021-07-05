9c92deb1c981c5798a8d5ca6831ece78eb5f2e02
Alex Wang <alexwangnj@gmail.com>
2021-07-05 14:41:09 -0400

    Create drawCanvas() method

--- src/components/interval-trainer/MusicDisplay.vue
+++ src/components/interval-trainer/MusicDisplay.vue
@@ -34,44 +34,7 @@ export default {
     }
   },
   mounted() {
-    let renderer = new VF.Renderer(document.getElementById("boo"), VF.Renderer.Backends.SVG);
-    let currentQuestion = this.getCurrentState().context.currentQuestion;
-    let noteName = currentQuestion.notes[0].getNoteName();
-
-    renderer.resize(625, 100);
-    let context = renderer.getContext();
-
-  // measure 1
-    var staveMeasure1 = new VF.Stave(10, 0, 300);
-    var notesMeasure1 = [];
-    var referenceNote;
-    staveMeasure1.addClef("treble").addTimeSignature("4/4").setContext(context).draw();
-
-    // need to account for other accidentals as well
-    if(noteName.search("#") != -1) {
-      referenceNote = new VF.StaveNote({clef: "treble", keys: [`${noteName}/4`], duration: "w" }).addAccidental(0, new VF.Accidental("#"))
-    } else {
-      referenceNote =  new VF.StaveNote({clef: "treble", keys: [`${noteName}/4`], duration: "w" });
-    }
-  notesMeasure1.push(referenceNote);
-
-  // Helper function to justify and draw a 4/4 voice
-  VF.Formatter.FormatAndDraw(context, staveMeasure1, notesMeasure1);
-
-  // measure 2 - juxtaposing second measure next to first measure
-  var staveMeasure2 = new VF.Stave(
-    staveMeasure1.width + staveMeasure1.x,
-    0,
-    275
-  );
-  staveMeasure2.setContext(context).draw();
-
-  // var notesMeasure2 = [
-  //   new VF.StaveNote({ keys: ["c/5"], duration: "w" }),
-  // ];
-
-  // VF.Formatter.FormatAndDraw(context, staveMeasure2, notesMeasure2);
-
+    this.drawCanvas();
   },
   methods: {
     ...mutations,
@@ -90,9 +53,40 @@ export default {
     parse() {
       var input = this.getCurrentState().context.userInput;
       console.log('current user input: ' + input);
-
-
       return input;
+    },
+    drawCanvas() {
+      let renderer = new VF.Renderer(document.getElementById("boo"), VF.Renderer.Backends.SVG);
+      let currentQuestion = this.getCurrentState().context.currentQuestion;
+      let noteName = currentQuestion.notes[0].getNoteName();
+
+      renderer.resize(625, 100);
+      let context = renderer.getContext();
+
+    // measure 1
+      var staveMeasure1 = new VF.Stave(10, 0, 300);
+      var notesMeasure1 = [];
+      var referenceNote;
+      staveMeasure1.addClef("treble").addTimeSignature("4/4").setContext(context).draw();
+
+      // need to account for other accidentals as well
+      if(noteName.search("#") != -1) {
+        referenceNote = new VF.StaveNote({clef: "treble", keys: [`${noteName}/4`], duration: "w" }).addAccidental(0, new VF.Accidental("#"))
+      } else {
+        referenceNote =  new VF.StaveNote({clef: "treble", keys: [`${noteName}/4`], duration: "w" });
+      }
+    notesMeasure1.push(referenceNote);
+
+    // Helper function to justify and draw a 4/4 voice
+    VF.Formatter.FormatAndDraw(context, staveMeasure1, notesMeasure1);
+
+    // measure 2 - juxtaposing second measure next to first measure
+    var staveMeasure2 = new VF.Stave(
+      staveMeasure1.width + staveMeasure1.x,
+      0,
+      275
+    );
+    staveMeasure2.setContext(context).draw();
     }
   }
 }
